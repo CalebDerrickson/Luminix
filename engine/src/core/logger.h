1f78422e3e02093b8efc975e92ed1773ab5d5ea1
Caleb Derrickson <Calebderrickson@gmail.com>
2023-09-01 13:45:57 -0500

    Logger implementation

---
+++ engine/src/core/logger.h
@@ -0,0 +1,71 @@
+#pragma once 
+#include "defines.h"
+
+// Details:
+// LOG_LEVEL_FATAL denotes a critical failure in the engine and will cause the engine to crash 
+// LOG_LEVEL_ERROR denotes an error has occured, the engine will still run, perhaps unpredictably
+// LOG_LEVEL_WARN  tells us of a suboptimal event which has occured
+// LOG_LEVEL_INFO  standard logging
+// LOG_LEVEL_DEBUG verbose details of code execution
+// LOG_LEVEL_TRACE far more verbose
+//
+// Both LOG_LEVEL_FATAL and LOG_LEVEL_ERROR are always on by default
+
+
+#define LOG_WARN_ENABLED 1
+#define LOG_INFO_ENABLED 1
+#define LOG_DEBUG_ENABLED 1
+#define LOG_TRACE_ENABLED 1
+
+// Disables debug and trace level logging on release
+#if RELEASE == 1
+#define LOG_DEBUG_ENABLED 0
+#define LOG_TRACE_ENABLED 0
+#endif
+
+
+typedef enum log_level{
+    LOG_LEVEL_FATAL = 0,
+    LOG_LEVEL_ERROR = 1,
+    LOG_LEVEL_WARN = 2,
+    LOG_LEVEL_INFO = 3,
+    LOG_LEVEL_DEBUG = 4, 
+    LOG_LEVEL_TRACE = 5
+} log_level;
+
+b8 initialize_logging();
+void shutdown_logging();
+
+KAPI void log_output(log_level level, const char* message, ...);
+
+#define KFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__);
+
+#ifndef KERROR
+#define KERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__);
+#endif
+
+#if LOG_WARN_ENABLED == 1
+#define KWARN(message, ...) log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__);
+#else
+#define KWARN(message, ...)
+#endif
+
+#if LOG_INFO_ENABLED == 1
+#define KINFO(message, ...) log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__);
+#else
+#define KINFO(message, ...)
+#endif
+
+
+#if LOG_DEBUG_ENABLED == 1
+#define KDEBUG(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
+#else
+#define KDEBUG(message, ...)
+#endif
+
+
+#if LOG_TRACE_ENABLED == 1
+#define KTRACE(message, ...) log_output(LOG_LEVEL_TRACE, message, ##__VA_ARGS__);
+#else
+#define KTRACE(message, ...)
+#endif
