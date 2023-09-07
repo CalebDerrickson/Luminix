92bdffa05f04861c10cd1f0dcaaeceb2a85ac03e
Caleb Derrickson <Calebderrickson@gmail.com>
2023-09-07 11:10:51 -0500

    Basic Windows input handling

--- engine/src/platform/platform_win32.c
+++ engine/src/platform/platform_win32.c
@@ -1,6 +1,7 @@
 #include "platform/platform.h"
 #include "core/logger.h"
 #include <stdio.h>
+#include "core/input.h"
 
 // Windows platform layer.
 #if KPLATFORM_WINDOWS
@@ -233,16 +234,21 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARA
         case WM_KEYUP:
         case WM_SYSKEYUP: {
             // Key pressed / released
-            // b8 pressed = (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN);
-            // TODO: Input processing
+            b8 pressed = (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN);
+            keys key = (u16)w_param;
+            
+            // Pass to input subsystem for processing
+            input_process_key(key, pressed);
 
         } break;
 
         case WM_MOUSEMOVE: {
             // Mouse move
-            // i32 x_position = GET_X_LPARAM(l_param);
-            // i32 y_position = GET_Y_LPARAM(l_param);
-            // TODO: Input processing
+            i32 x_position = GET_X_LPARAM(l_param);
+            i32 y_position = GET_Y_LPARAM(l_param);
+            
+            // Pass over to input subsystem
+            input_process_mouse_move(x_position, y_position);
             
         } break;
 
@@ -250,10 +256,13 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARA
             i32 z_delta = GET_WHEEL_DELTA_WPARAM(w_param);
             if(z_delta != 0) {
                 // Flatten the input to be OS - independent (-1, 1)
-                // z_delta = (z_delta <= 0) ? -1 : 1;
-            }
-            // TODO: Input processing
+                // Not checking for variable wheel speeds.
+                z_delta = (z_delta <= 0) ? -1 : 1;
 
+                // Pass to input subsystem
+                input_process_mouse_wheel(z_delta);
+            }
+            
         } break;
     
         case WM_LBUTTONDOWN:
@@ -262,9 +271,32 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARA
         case WM_LBUTTONUP:
         case WM_MBUTTONUP:
         case WM_RBUTTONUP: {
-            // b8 pressed = msg == WM_LBUTTONDOWN || msg == WM_MBUTTONDOWN || msg == WM_RBUTTONDOWN; 
-            // TODO: Input processing
+            // Above are handled together, then filtered 
+            b8 pressed = msg == WM_LBUTTONDOWN || msg == WM_MBUTTONDOWN || msg == WM_RBUTTONDOWN; 
+            buttons mouse_button = BUTTON_MAX_BUTTONS;
 
+            switch (msg)
+            {
+            case WM_LBUTTONDOWN:
+            case WM_LBUTTONUP:
+                mouse_button = BUTTON_LEFT;
+                break;
+
+            case WM_MBUTTONDOWN:
+            case WM_MBUTTONUP:
+                mouse_button = BUTTON_MIDDLE;
+                break;
+
+            case WM_RBUTTONDOWN:
+            case WM_RBUTTONUP:
+                mouse_button = BUTTON_RIGHT;
+                break;
+            }
+
+            // Pass to input subsystem
+            if(mouse_button != BUTTON_MAX_BUTTONS) {
+                input_process_button(mouse_button, pressed);
+            }
         } break;
 
     }
