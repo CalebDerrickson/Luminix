defb3fa3a5c493645a3d5fc551ab31c65cb26e86
Caleb Derrickson <Calebderrickson@gmail.com>
2023-09-16 23:32:15 -0500

    update

--- engine/src/renderer/renderer_frontend.c
+++ engine/src/renderer/renderer_frontend.c
@@ -3,6 +3,7 @@
 
 #include "core/logger.h"
 #include "core/lmemory.h"
+#include "math/lmath.h"
 
 typedef struct renderer_system_state {
     renderer_backend backend;
@@ -72,7 +73,15 @@ b8 renderer_draw_frame(render_packet* packet)
 {
     // If the begin frame returned successfully, mid-frame operations may continue.
     if (renderer_begin_frame(packet->delta_time)) {
+        mat4 projection = mat4_perspective(deg_to_rad(45.0f), 1280/720.0f, 0.1f, 1000.0f);
+        static f32 z = -1.0f;
 
+        if(z > -10.0f){
+            z -= 0.001f;
+        }
+        
+        mat4 view = mat4_translation((vec3){0, 0, z});
+        state_ptr->backend.update_global_state(projection, view, vec3_set(0), vec4_set(1.0f), 0);
         // End the frame if this fails, likely unrecoverable.
         b8 result = renderer_end_frame(packet->delta_time);
 
--- engine/src/renderer/renderer_frontend.h
+++ engine/src/renderer/renderer_frontend.h
@@ -4,6 +4,7 @@
 
 
 b8 renderer_system_initialize(u64* memory_requirement, void* state, const char* application_name);
+
 void renderer_system_shutdown(void* state);
 
 void renderer_on_resized(u16 width, u16 height);
