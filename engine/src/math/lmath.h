68d560eeaf7d18cfc07cb344075f558828c6ebf6
Caleb Derrickson <Calebderrickson@gmail.com>
2024-03-13 19:00:14 -0500

    Minor changes

--- engine/src/math/lmath.h
+++ engine/src/math/lmath.h
@@ -5,18 +5,18 @@
 
 #include "core/lmemory.h"
 
-#define K_PI 3.14159265358979323846f
-#define K_PI_2 2.0f * K_PI
-#define K_HALF_PI 0.5f * K_PI
-#define K_QUARTER_PI 0.25f * K_PI
-#define K_ONE_OVER_PI 1.0f / K_PI
-#define K_ONE_OVER_TWO_PI 1.0f / K_PI_2
-#define K_SQRT_TWO 1.41421356237309504880f
-#define K_SQRT_THREE 1.73205080756887729352f
-#define K_SQRT_ONE_OVER_TWO 0.70710678118654752440f
-#define K_SQRT_ONE_OVER_THREE 0.57735026918962576450f
-#define K_DEG2RAD_MULTIPLIER K_PI / 180.0f
-#define K_RAD2DEG_MULTIPLIER 180.0f / K_PI
+#define L_PI 3.14159265358979323846f
+#define L_PI_2 2.0f * L_PI
+#define L_HALF_PI 0.5f * L_PI
+#define L_QUARTER_PI 0.25f * L_PI
+#define L_ONE_OVER_PI 1.0f / L_PI
+#define L_ONE_OVER_TWO_PI 1.0f / L_PI_2
+#define L_SQRT_TWO 1.41421356237309504880f
+#define L_SQRT_THREE 1.73205080756887729352f
+#define L_SQRT_ONE_OVER_TWO 0.70710678118654752440f
+#define L_SQRT_ONE_OVER_THREE 0.57735026918962576450f
+#define L_DEG2RAD_MULTIPLIER L_PI / 180.0f
+#define L_RAD2DEG_MULTIPLIER 180.0f / L_PI
 
 // The multiplier to convert seconds to milliseconds.
 #define K_SEC_TO_MS_MULTIPLIER 1000.0f
@@ -1292,7 +1292,7 @@ LINLINE quat quat_slerp(quat q_0, quat q_1, f32 percentage) {
  * @return The amount in radians.
  */
 LINLINE f32 deg_to_rad(f32 degrees) {
-    return degrees * K_DEG2RAD_MULTIPLIER;
+    return degrees * L_DEG2RAD_MULTIPLIER;
 }
 
 /**
@@ -1302,5 +1302,5 @@ LINLINE f32 deg_to_rad(f32 degrees) {
  * @return The amount in degrees.
  */
 LINLINE f32 rad_to_deg(f32 radians) {
-    return radians * K_RAD2DEG_MULTIPLIER;
+    return radians * L_RAD2DEG_MULTIPLIER;
 }
